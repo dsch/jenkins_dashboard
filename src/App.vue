2c0ba327ba95e392ed159ba7de74afa5e356dcdf
David Schneider <schneidav81@gmail.com>
2018-09-01 21:11:40 +0200

    last changes

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,32 @@ function fetchJenkinsJsonApi (url) {
     })
 }
 
+function fetchAllJobs (vm) {
+  fetchJenkinsJsonApi(vm.url)
+    .then(function (data) {
+      vm.jobs = data.jobs
+        .filter(function (job) {
+          return job.color !== 'disabled'
+        })
+        .map(function (job) {
+          const newJob = {
+            name: job.name,
+            url: job.url,
+            status: 'loading'
+          }
+          fetchJenkinsJsonApi(newJob.url + '/lastBuild')
+            .then(function (build) {
+              updateJobStatus(newJob, build)
+              vm.jobs = vm.jobs.sort(function (a, b) {
+                return b.date - a.date
+              })
+            })
+          return newJob
+        })
+    })
+  setTimeout(fetchAllJobs, 15000, vm)
+}
+
 export default {
   name: 'App',
   components: {
@@ -43,35 +69,13 @@ export default {
   data () {
     return {
       // url: 'https://jenkins.mono-project.com/view/Components',
-      url: 'https://jenkins.mono-project.com/',
-      // url: '/static/jenkins/view',
+      // url: 'https://jenkins.mono-project.com/',
+      url: '/static/jenkins/view',
       jobs: [ ]
     }
   },
   created: function () {
-    const vm = this
-    fetchJenkinsJsonApi(vm.url)
-      .then(function (data) {
-        vm.jobs = data.jobs
-          .filter(function (job) {
-            return job.color !== 'disabled'
-          })
-          .map(function (job) {
-            const newJob = {
-              name: job.name,
-              url: job.url,
-              status: 'loading'
-            }
-            fetchJenkinsJsonApi(newJob.url + '/lastBuild')
-              .then(function (build) {
-                updateJobStatus(newJob, build)
-                vm.jobs = vm.jobs.sort(function (a, b) {
-                  return b.date - a.date
-                })
-              })
-            return newJob
-          })
-      })
+    fetchAllJobs(this)
   }
 }
 </script>
